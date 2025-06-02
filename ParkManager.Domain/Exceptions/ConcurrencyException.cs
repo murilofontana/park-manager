9067a9db2baefa94c0bf9a2eed38060b16c2db2d
Murilo Fontana <fontana.murilo@gmail.com>
2025-06-02 20:01:31 -0300

    adding custom exceptions

---
+++ ParkManager.Domain/Exceptions/ConcurrencyException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace ParkManager.Domain.Exceptions;
+
+public class ConcurrencyException : Exception
+{
+  public ConcurrencyException()
+  {
+  }
+
+  public ConcurrencyException(string? message) : base(message)
+  {
+  }
+
+  public ConcurrencyException(string? message, Exception? innerException) : base(message, innerException)
+  {
+  }
+
+  protected ConcurrencyException(SerializationInfo info, StreamingContext context) : base(info, context)
+  {
+  }
+}
