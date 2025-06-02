a409796a5294b75f5d5b7fed128fe984af702533
Murilo Fontana <fontana.murilo@gmail.com>
2025-06-02 19:30:16 -0300

    adding result parttern

---
+++ ParkManager.Domain/Abstraction/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkManager.Domain.Abstraction
+{
+  public record Error(string Code, string Name)
+  {
+    public static Error None = new(string.Empty, string.Empty);
+
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+  }
+
+}
