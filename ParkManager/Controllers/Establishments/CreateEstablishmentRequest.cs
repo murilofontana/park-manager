4cfe927f0ac3e72d21e362ddb153db82bc3b7a24
Murilo Fontana <fontana.murilo@gmail.com>
2025-06-02 19:41:41 -0300

    adding controller to establishment and the create action

---
+++ ParkManager/Controllers/Establishments/CreateEstablishmentRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace ParkManager.Controllers.Establishments
+{
+  public record CreateEstablishmentRequest
+  {
+    public string Name { get; set; }
+    public string Cnpj { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string Complement { get; set; }
+    public string ZipCode { get; set; }
+    public string Phone { get; set; }
+    public int MotorcyclesParkingSpaces { get; set; }
+    public int CarsParkingSpaces { get; set; }
+  }
+}
---
+++ ParkManager/Controllers/Establishments/EstablishmentController.cs
@@ -0,0 +1,74 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ParkManager.Application.Establishments.Create;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParkManager.Controllers.Establishments
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EstablishmentController : ControllerBase
+  {
+    private readonly ISender _sender;
+
+    public EstablishmentController(ISender sender)
+    {
+      _sender = sender;
+    }
+
+    // GET: api/<EstablishmentController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<EstablishmentController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/<EstablishmentController>
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateEstablishmentRequest value)
+    {
+      var command = new CreateEstablishmentCommand(
+        value.Name,
+        value.Cnpj,
+        value.City,
+        value.State,
+        value.Street,
+        value.Number,
+        value.Complement,
+        value.ZipCode,
+        value.Phone,
+        value.MotorcyclesParkingSpaces,
+        value.CarsParkingSpaces
+      );
+
+      var result = await _sender.Send(command);
+
+      if (result.IsFailure)
+      {
+        return BadRequest(result.Error);
+      }
+
+      return Ok(result.Value);
+    }
+
+    // PUT api/<EstablishmentController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<EstablishmentController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
