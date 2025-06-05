ddb3590ed691d31b2a846b883cea7749d2602fce
Murilo Fontana <fontana.murilo@gmail.com>
2025-06-05 20:21:51 -0300

    Remove two methods from IEstablishmentRepository interface
    The methods `GetTotalGeneralEntry` and `GetTotalGeneralExit`
    were removed from the `IEstablishmentRepository` interface
    in `IEstablishmentRepository.cs`. No new methods were added,
    and the rest of the interface remains unchanged.

--- ParkManager.Application/Common/IEstablishmentRepository.cs
+++ ParkManager.Application/Common/IEstablishmentRepository.cs
@@ -10,8 +10,6 @@ public interface IEstablishmentRepository
   Task<Establishment> GetByIdAsync(Guid id, CancellationToken cancellationToken);
   Task<int> GetTotalCarsEntry(Guid establishmentId);
   Task<int> GetTotalCarsExit(Guid establishmentId);
-  Task<int> GetTotalGeneralEntry(Guid establishmentId);
-  Task<int> GetTotalGeneralExit(Guid establishmentId);
   Task<int> GetTotalMotorcyclesEntry(Guid establishmentId);
   Task<int> GetTotalMotorcyclesExit(Guid establishmentId);
   Task UpdateAsync(Establishment establishment, CancellationToken cancellationToken);
